97a8f865f9a906d1c75781edb4e791385705a9ad
liudehua <liudehua@163.com>
2022-07-20 15:57:05 +0800

    add  setting config.

--- src/tgin/main.go
+++ src/tgin/main.go
@@ -2,18 +2,25 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"tgin/bootstrap"
+	"tgin/global"
 	"net/http"
 )
 
 func main() {
+
+	bootstrap.InitializeConfig()
+
 	r := gin.Default()
 
 	// do test route
-	r.GET("/ping", func(c *gin.Context) {
-		c.String(http.StatusOK, "pong")
+	r.GET("/hello", func(c *gin.Context) {
+		c.String(http.StatusOK, "hello world!")
 	})
 
-	r.Run(":8088")
+	//r.Run(":8088")
+	r.Run(":" + global.App.Config.App.Port)
+
 }
 
 
